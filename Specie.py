a85748a39365ddfcb333f746e55050cfd71a234d
phi_Line <phi.liney@gmail.com>
2017-02-18 13:43:18 -0800

    build pop funct added in Specie.py

--- Specie.py
+++ Specie.py
@@ -1,34 +1,51 @@
 from random import *
+from Critter import *
 
 class Specie:
-    SIZE_MIN = 0.1
-    SIZE_MAX = 1.0
-    SPEED_MIN = 0.1
-    SPEED_MAX = 1.0
+    SPEED_MIN, SPEED_MAX = 0.1, 1.0
+    SIZE_MIN, SIZE_MAX = 1, 5
+    FOOD_MIN, FOOD_MAX = 0.1, 1
+    TURN_SPEED_MIN, TURN_SPEED_MAX = 30, 45
+    DETECT_MIN, DETECT_MAX = 1, 5
+    MIN_INDIVIDUALS, MAX_INDIVIDUALS = 5, 10
 
     def __init__(self, *args, **kwargs):
-
         self.speed  = 1 # starts as 1 (size per second)
         self.startSize = 1 # starting food
         self.foodDecayRate = .01 # .01 speed
         self.divideSize = 1.5 * self.startSize # 1.5 * start size
         self.turnSpeedVector = 45 # deg / sec (45 / sec)
-        self.detectDistance = self.startSize + (2 * self.startSize)
+        self.detectDistance = 2
         self.directionVector = [0,0]
         self.color = "#FF0000"
         self.typeName = "species_name"
         self.individuals = 10
         self.birthNumber = 0
+        population = []
 
     def random_initializer(self):
-        self.speed = uniform(0.5 1.5)
-        self.startSize = uniform(1, 5)
-        self.foodDecayRate = uniform(0.1, 1)
-        self.divideSize = uniform(, Specie.SIZE_MAX)
-        self.turnSpeedVector = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
-        self.detectDistance = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
-        self.directionVector = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
-        self.color = "#FF0000"
-        self.typeName = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
-        self.individuals = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
-        self.birthNumber = 0
+        self.speed = uniform(Specie.SPEED_MIN, Specie.SPEED_MAX)
+        self.startSize = uniform(Specie.SPEED_MIN, Specie.SPEED_MAX)
+        self.foodDecayRate = uniform(Specie.FOOD_MIN, Specie.FOOD_MAX)
+        self.divideSize = 1.5 * self.startSize
+        self.turnSpeedVector = uniform(Specie.TURN_SPEED_MIN, Specie.TURN_SPEED_MAX)
+        self.detectDistance = uniform(Specie.DETECT_MIN, Specie.DETECT_MAX)
+        r = lambda: random.randint(0, 255)
+        self.color = '#%02X%02X%02X' % (r(),r(),r())
+        self.individuals = uniform(Specie.MIN_INDIVIDUALS, Specie.MIN_INDIVIDUALS)
+
+    #fucntion that builds a list
+    def build_pop(self):
+        '''
+        this function fills the member population with a seed gen of x Critters
+        :return: void
+        '''
+        for i in range(0, self.individuals):
+            this_critter = Critter()
+            self.population[0] = this_critter
+
+    #class variable that builds population
+    #birth new members based on div size
+    def determine_birth(self):
+        for
+    #remove dead members
