4dfaf3d6a95b921c9df4eb9028b01914e23e5ae5
phi_Line <phi.liney@gmail.com>
2017-02-18 12:57:12 -0800

    lol this is a bad commit

--- Specie.py
+++ Specie.py
@@ -1,13 +1,34 @@
+from random import *
+
 class Specie:
+    SIZE_MIN = 0.1
+    SIZE_MAX = 1.0
+    SPEED_MIN = 0.1
+    SPEED_MAX = 1.0
+
     def __init__(self, *args, **kwargs):
-        self.speed
-        self.foodDecayRate
-        self.divideSize
-        self.turnSpeedVector
-        self.detectDistance
-        self.directionVector
-        self.color
-        self.typeName
-        self.speed
-        self.individuals
+
+        self.speed  = 1 # starts as 1 (size per second)
+        self.startSize = 1 # starting food
+        self.foodDecayRate = .01 # .01 speed
+        self.divideSize = 1.5 * self.startSize # 1.5 * start size
+        self.turnSpeedVector = 45 # deg / sec (45 / sec)
+        self.detectDistance = self.startSize + (2 * self.startSize)
+        self.directionVector = [0,0]
+        self.color = "#FF0000"
+        self.typeName = "species_name"
+        self.individuals = 10
+        self.birthNumber = 0
+
+    def random_initializer(self):
+        self.speed = uniform(0.5 1.5)
+        self.startSize = uniform(1, 5)
+        self.foodDecayRate = uniform(0.1, 1)
+        self.divideSize = uniform(, Specie.SIZE_MAX)
+        self.turnSpeedVector = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
+        self.detectDistance = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
+        self.directionVector = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
+        self.color = "#FF0000"
+        self.typeName = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
+        self.individuals = uniform(Specie.SIZE_MIN, Specie.SIZE_MAX)
         self.birthNumber = 0
